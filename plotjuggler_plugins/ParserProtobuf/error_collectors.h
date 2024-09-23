9c847649a46c22952d356071de12b0fb469e5202
João Freitas <joaj.freitas@gmail.com>
2024-09-23 19:21:04 +0100

    Use RecordError and RecordWarning
    AddError and AddWarning have been deprecated: https://github.com/protocolbuffers/protobuf/commit/543fbcdbd9496de9d93a6eb4645640cb47a5514b.
    
    Instead RecordError and RecordWarning shall be used.

--- plotjuggler_plugins/ParserProtobuf/error_collectors.h
+++ plotjuggler_plugins/ParserProtobuf/error_collectors.h
@@ -7,14 +7,16 @@
 
 #include <QStringList>
 
+#include "absl/strings/string_view.h"
+
 class IoErrorCollector: public google::protobuf::io::ErrorCollector
 {
 public:
-  void AddError(int line, google::protobuf::io::ColumnNumber column,
-                const std::string& message) override;
+  void RecordError(int line, google::protobuf::io::ColumnNumber column,
+                const absl::string_view message) override;
 
-  void AddWarning(int line, google::protobuf::io::ColumnNumber column,
-                  const std::string& message) override;
+  void RecordWarning(int line, google::protobuf::io::ColumnNumber column,
+                  const absl::string_view message) override;
 
   const QStringList& errors()
   {
@@ -28,11 +30,11 @@ private:
 class FileErrorCollector: public google::protobuf::compiler::MultiFileErrorCollector
 {
 public:
-  void AddError(const std::string& filename, int line, int,
-                const std::string& message) override;
+  void RecordError(const absl::string_view filename, int line, int,
+                const absl::string_view message) override;
 
-  void AddWarning(const std::string& filename, int line, int,
-                  const std::string& message) override;
+  void RecordWarning(const absl::string_view filename, int line, int,
+                  const absl::string_view message) override;
 
   const QStringList& errors()
   {
