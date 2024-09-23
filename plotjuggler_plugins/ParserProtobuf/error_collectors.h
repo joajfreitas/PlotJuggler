93616d019c83a2ed33dec2d6973b2ed229f5639f
João Freitas <joaj.freitas@gmail.com>
2024-09-23 19:21:04 +0100

    Use RecordError and RecordWarning
    AddError and AddWarning have been deprecated: https://github.com/protocolbuffers/protobuf/commit/543fbcdbd9496de9d93a6eb4645640cb47a5514b.
    
    Instead RecordError and RecordWarning shall be used.

---
+++ plotjuggler_plugins/ParserProtobuf/error_collectors.h
@@ -0,0 +1,48 @@
+#ifndef ERROR_COLLECTORS_H
+#define ERROR_COLLECTORS_H
+
+
+#include <google/protobuf/io/tokenizer.h>
+#include <google/protobuf/compiler/importer.h>
+
+#include <QStringList>
+
+#include "absl/strings/string_view.h"
+
+class IoErrorCollector: public google::protobuf::io::ErrorCollector
+{
+public:
+  void RecordError(int line, google::protobuf::io::ColumnNumber column,
+                const absl::string_view message) override;
+
+  void RecordWarning(int line, google::protobuf::io::ColumnNumber column,
+                  const absl::string_view message) override;
+
+  const QStringList& errors()
+  {
+    return _errors;
+  }
+
+private:
+  QStringList _errors;
+};
+
+class FileErrorCollector: public google::protobuf::compiler::MultiFileErrorCollector
+{
+public:
+  void RecordError(const absl::string_view filename, int line, int,
+                const absl::string_view message) override;
+
+  void RecordWarning(const absl::string_view filename, int line, int,
+                  const absl::string_view message) override;
+
+  const QStringList& errors()
+  {
+    return _errors;
+  }
+
+private:
+  QStringList _errors;
+};
+
+#endif // ERROR_COLLECTORS_H
